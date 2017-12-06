1fbc06e0997010b540996c38506765e3d7227c03
beesperester <bernhard@esperester.de>
2017-12-06 14:55:05 +0100

    rename module

---
+++ exrio/__main__.py
@@ -0,0 +1,43 @@
+""" Main python module. """
+
+# system
+import logging
+
+# filesystem
+from fs.osfs import OSFS
+from fs.errors import CreateFailed
+
+# exrio
+from exrio import rename_dir
+
+def main():
+    # try:
+        
+    # except CreateFailed as error:
+    #     print error
+    in_fs = OSFS(u'C:/Users/Bernhard Esperester/Desktop/wsds/scenes/UFA_WSDS_SC0110_S0010/img-cgi/lighting_UFA_WSDS_SC0110_S0010_v006_beresp/final')
+    out_fs = OSFS(u'C:/Users/Bernhard Esperester/Desktop/wsds/scenes/UFA_WSDS_SC0110_S0010/img-cgi/lighting_UFA_WSDS_SC0110_S0010_v006_beresp/test_rename')
+
+    layer_map = {
+        r'^r$': 'R',
+        r'^g$': 'G',
+        r'^b$': 'B',
+        r'^a$': 'A',
+        r'diffuse': 'diffuse',
+        r'shadow': 'shadow',
+        r'ambient': 'emission',
+        r'global illumination': 'gi',
+        r'ambient occlusion': 'ao',
+        r'reflection': 'reflection',
+        r'refraction': 'refraction',
+        r'normal pass': 'normal',
+        r'position \(world\)': 'position_world',
+        r'position \(camera\)': 'position_camera'
+    }
+
+    rename_dir(in_fs, out_fs, layer_map, None, 16)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    main()
--- exrio/exrio.py
+++ exrio/exrio.py
@@ -14,10 +14,6 @@ from threading import Thread
 # exr
 import OpenEXR
 
-# filesystem
-from fs.osfs import OSFS
-from fs.errors import CreateFailed
-
 class NoExrFileException(Exception):
     """ No EXR file exception. """
 
@@ -191,35 +187,3 @@ def rename_dir(in_fs, out_fs, layer_map=None, rename_method=None, num_threads=8)
         files.append(in_fs.getsyspath(file_name))
 
     return rename_files(files, out_fs, layer_map, rename_method, num_threads)
-
-def main():
-    # try:
-        
-    # except CreateFailed as error:
-    #     print error
-    in_fs = OSFS(u'C:/Users/Bernhard Esperester/Desktop/wsds/scenes/UFA_WSDS_SC0110_S0010/img-cgi/lighting_UFA_WSDS_SC0110_S0010_v006_beresp/final')
-    out_fs = OSFS(u'C:/Users/Bernhard Esperester/Desktop/wsds/scenes/UFA_WSDS_SC0110_S0010/img-cgi/lighting_UFA_WSDS_SC0110_S0010_v006_beresp/test_rename')
-
-    layer_map = {
-        r'^r$': 'R',
-        r'^g$': 'G',
-        r'^b$': 'B',
-        r'^a$': 'A',
-        r'diffuse': 'diffuse',
-        r'shadow': 'shadow',
-        r'ambient': 'emission',
-        r'global illumination': 'gi',
-        r'ambient occlusion': 'ao',
-        r'reflection': 'reflection',
-        r'refraction': 'refraction',
-        r'normal pass': 'normal',
-        r'position \(world\)': 'position_world',
-        r'position \(camera\)': 'position_camera'
-    }
-
-    rename_dir(in_fs, out_fs, layer_map, None, 16)
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-
-    main()
