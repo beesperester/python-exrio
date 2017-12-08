372b4bde1275283ccb33a89b3afcc3350ba3ecb3
beesperester <bernhard@esperester.de>
2017-12-08 11:46:39 +0100

    format console output

--- exrio/console.py
+++ exrio/console.py
@@ -1,7 +1,7 @@
 """ Console output module. """
 
 def _output_args(*args):
-    print ' '.join([repr(arg).strip('\'') for arg in args])
+    print '\t'.join([repr(arg).strip('\'') for arg in args])
 
 def error(*args):
     _output_args('ERROR:', *args)
